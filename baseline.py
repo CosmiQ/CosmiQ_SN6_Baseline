d9079c0b565e64408c2814b8d0903ac3d37339ed
Daniel Hogan <dhogan@iqt.org>
2020-02-10 21:24:58 +0000

    Common file for training and testing

---
+++ baseline.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+
+import solaris as sol
+
+def test(inputargs):
+    parser = argparse.ArgumentParser(description='SpaceNet 6: Baseline Algorithm Testing')
+    
+
+if __name__ == '__main__':
+    test(sys.argv[1:])
