8a4932d343e4b548512d68ec3d93c55c16f5dc99
Daniel Hogan <dhogan@iqt.org>
2020-02-10 22:09:26 +0000

    Scripts for training and testing

---
+++ train.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./baseline.py --pretrain --train --sardir /nfs/data/cosmiq/spacenet/competitions/SN6_buildings/Distributable/train/SAR-Intensity --labeldir /nfs/data/cosmiq/spacenet/competitions/SN6_buildings/Distributable/train/Buildings --maskdir ~/wdata/rotterdam/baseline_data/masks
