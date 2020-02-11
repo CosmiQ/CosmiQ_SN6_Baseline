4bd778f1918ba45e863f36a79bc7f7841da0bed7
Daniel Hogan <dhogan@iqt.org>
2020-02-11 14:06:34 -0500

    Makes raster label masks

--- baseline.py
+++ baseline.py
@@ -1,26 +1,56 @@
 #!/usr/bin/env python
 
+import os
 import sys
+import glob
 import argparse
+import tqdm
 
 import solaris as sol
 
+
 def pretrain(args):
+    """
+    Creates formatted versions of data used for training,
+    including raster label masks.
+    """
+    print('Pretrain')
     assert(args.sardir is not None and args.labeldir is not None and args.maskdir is not None)
-    print('pretrain')
+
+    #Get paths to relevant files
+    sarpaths = glob.glob(os.path.join(args.sardir, '*.tif'))
+    tilepaths.sort()
+    labelpaths = glob.glob(os.path.join(args.labeldir, '*.geojson'))
+    labelpaths.sort()
+    maskpaths = [os.path.join(args.maskdir, os.path.basename(tilepath)) for tilepath in tilepaths]
+
+    #Create empty folder to hold masks
+    pathlib.Path(args.maskdir).mkdir(exist_ok=True)
+    oldmasks = glob.glob(os.path.join(args.maskdir, '*.tif'))
+    for oldmask in oldmasks:
+        os.remove(oldmask)
+
+    #Create and save masks
+    for i, (sarpath, labelpath, maskpath) in tqdm.tqdm(enumerate(zip(sarpaths, labelpaths, maskpaths)), total=len(sarpaths)):
+        gdf = gpd.read_file(labelpath)
+        maskdata = sol.vector.mask.footprint_mask(
+            df=gdf,
+            reference_im=sarpath,
+            out_file=maskpath
+        )
 
 def train(args):
-    print('train')
+    print('Train')
 
 def pretest(args):
-    print('pretest')
+    print('Pretest')
     
 def test(args):
-    print('test')
+    print('Test')
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='SpaceNet 6: Baseline Algorithm Training')
+    parser = argparse.ArgumentParser(description='SpaceNet 6 Baseline Algorithm')
     #Which operations to carry out
     parser.add_argument('--pretrain', action='store_true',
                         help='Whether to format training data')
