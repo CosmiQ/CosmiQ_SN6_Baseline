7c39a77c269424b9d4dfd0a5d6986abd53927959
Daniel Hogan <dhogan@iqt.org>
2020-02-12 21:36:47 +0000

    Outline of copying-with-rotation function

--- baseline.py
+++ baseline.py
@@ -3,11 +3,13 @@
 import os
 import sys
 import glob
+import uuid
 import shutil
 import pathlib
 import argparse
 import pandas as pd
 import geopandas as gpd
+import gdal
 import tqdm
 
 import solaris as sol
@@ -25,9 +27,6 @@ def readrotationfile(path):
     """
     Reads SAR_orientations file, which lists whether each strip was imaged
     from the north (denoted by 0) or from the south (denoted by 1).
-
-    Example of using the resulting dataframe:
-    rotationdf.loc['20190823161251_20190823161546'].squeeze()
     """
     rotationdf = pd.read_csv(args.rotationfile,
                              sep=' ',
@@ -37,11 +36,51 @@ def readrotationfile(path):
     rotationdf['direction'] = rotationdf['direction'].astype(int)
     return rotationdf
 
+
 def lookuprotation(tilepath, rotationdf):
     """
     Looks up the SAR_orientations value for a tile based on its filename
     """
-    pass
+    tilename = os.path.splitext(os.path.basename(tilepath))[0]
+    stripname = '_'.join(tilename.split('_')[-4:-2])
+    rotation = rotationdf.loc[stripname].squeeze()
+    return rotation
+
+
+def copyimage(srcpath, dstpath, rotate=False, deletesource=False):
+    """
+    Copying with rotation:  Copies a TIFF image from srcpath to dstpath,
+    rotating the image by 180 degrees if specified.  If srcpath and dstpath
+    are the same, rotation is done in place.
+    """
+    #Handles special case where source path and destination path are the same
+    if srcpath==dstpath:
+        if not rotate:
+            #Then there's nothing to do
+            return
+        else:
+            #Move file to temporary location
+            srcpath = srcpath + str(uuid.uuid4())
+            shutil.move(dstpath, srcpath)
+            deletesource = True
+
+    if not rotate:
+        shutil.copy(srcpath, dstpath, follow_symlinks=True)
+    else:
+        #Rotate image with gdal
+        driver = gdal.GetDriverByName('GTiff')
+
+        #Read in tile
+        tilefile = gdal.Open(srcpath)
+        geotransform = tilefile.GetGeoTransform()
+        projection = tilefile.GetProjection()
+        tileband = tilefile.GetRasterBand(1)
+        tilevals = tileband.ReadAsArray()
+        del tileband
+        del tilefile
+
+    if deletesource:
+        os.remove(srcpath)
 
 
 def pretrain(args):
@@ -66,7 +105,7 @@ def pretrain(args):
     for folder in folders:
         makeemptyfolder(folder)
 
-    #Rotate masks and images, if enabled
+    #Look up how to rotate masks and images, if enabled
     if args.rotate:
         assert(args.rotationfile is not None)
         rotationdf = readrotationfile(args.rotationfile)
@@ -84,6 +123,14 @@ def pretrain(args):
             reference_im=sarpath,
             out_file=maskpath
         )
+        if args.rotate:
+            rotationflag = lookuprotation(sarpath, rotationdf)
+        else:
+            rotationflag = 0
+        if rotationflag==1:
+            #Rotate
+            pass
+
         if i>5:
             break
 
