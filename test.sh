ac425cca5a15a5e59902b075f1df49b1e1871b0e
Daniel Hogan <dhogan@iqt.org>
2020-02-17 16:42:41 -0500

    Copied file paths from train.sh to test.sh

---
+++ test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+srcdir=/nfs/data/cosmiq/spacenet/competitions/SN6_buildings/Distributable
+dstdir=~/wdata/rotterdam/baseline_data
+
+./baseline.py --pretest --sardir $srcdir/train/SAR-Intensity --opticaldir $srcdir/train/PS-RGB --labeldir $srcdir/train/Buildings --rotationfile $srcdir/train/SummaryData/SAR_orientations.txt --maskdir $dstdir/masks --sarprocdir $dstdir/sartrain --opticalprocdir $dstdir/optical --sartestdir $dstdir/sartest --traincsv $dstdir/train.csv --validcsv $dstdir/valid.csv --testcsv $dstdir/test.csv --yamlpath $dstdir/sar.yaml --modeldir $dstdir/models --testoutdir $dstdir/inference_output --rotate --mintrainsize 20
